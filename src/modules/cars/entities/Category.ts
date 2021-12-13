61545aae26f6da1ea3f882a49ab3966512ebc045
Guilherme Rocha <GuiPython2001@gmail.com>
2021-12-13 16:04:03 -0300

    refactor: Change models for entities typeorm  Category | Specification

---
+++ src/modules/cars/entities/Category.ts
@@ -0,0 +1,25 @@
+import { v4 as uuidv4 } from "uuid";
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from "typeorm";
+
+@Entity("categories")
+class Category {
+    @PrimaryColumn()
+    id?: string;
+
+    @Column()
+    name: string;
+
+    @Column()
+    description: string;
+
+    @CreateDateColumn()
+    created_at: Date;
+
+    constructor() {
+        if (!this.id) {
+            this.id = uuidv4();
+        }
+    }
+}
+
+export { Category };
---
+++ src/modules/cars/entities/Specification.ts
@@ -0,0 +1,25 @@
+import { v4 as uuidv4 } from "uuid";
+import { Column, Entity, CreateDateColumn, PrimaryColumn} from "typeorm"
+
+@Entity("specifications")
+class Specification {
+    @PrimaryColumn()
+    id?: string;
+
+    @Column()
+    name: string;
+
+    @Column()
+    description: string;
+
+    @CreateDateColumn()
+    created_at: Date;
+
+    constructor() {
+        if (!this.id) {
+            this.id = uuidv4();
+        }
+    }
+}
+
+export { Specification };
