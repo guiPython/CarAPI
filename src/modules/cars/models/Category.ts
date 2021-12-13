430a470b5e8e5c4ba7313f2ab7adad390ead40ee
Guilherme Rocha <GuiPython2001@gmail.com>
2021-12-12 21:13:59 -0300

    refactor: Change model Category, add typeorm decorators

--- src/modules/cars/models/Category.ts
+++ src/modules/cars/models/Category.ts
@@ -1,9 +1,18 @@
 import { v4 as uuidv4 } from "uuid";
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from "typeorm";
 
+@Entity("categories")
 class Category {
+    @PrimaryColumn()
     id?: string;
+
+    @Column()
     name: string;
+
+    @Column()
     description: string;
+
+    @CreateDateColumn()
     created_at: Date;
 
     constructor() {
