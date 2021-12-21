d277a36d711afb2b2045ece363cf6af4b2c5ec90
Guilherme Rocha <GuiPython2001@gmail.com>
2021-12-21 14:20:05 -0300

    feat: Add middleware for errors

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,30 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+import "reflect-metadata";
+import "express-async-errors";
 
-import { router } from "./modules/cars/routes";
-
-import "./database"
+import { AppError } from "./errors/AppError";
+import { router } from "./routes";
+import "./database";
+import "./shared/container";
 
 const app = express();
 app.use(express.json());
+
 app.use(router);
 
-app.listen(3333);
+app.use(
+    (err: Error, request: Request, response: Response, next: NextFunction) => {
+        if (err instanceof AppError) {
+            return response.status(err.statusCode).json({
+                message: err.message,
+            });
+        }
+
+        return response.status(500).json({
+            status: "error",
+            message: `Internal server error ${err.message}`,
+        });
+    }
+);
+
+app.listen(3333, () => console.log("Server running"));
